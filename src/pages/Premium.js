13bbbb64c40874f506e54c11d3b91e0618bf7cd1
rohmatmret <rohmat771@gmail.com>
2021-08-16 09:37:42 +0700

    set static url to env

--- src/pages/Premium.js
+++ src/pages/Premium.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
 import axios from "axios";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 /**
  *
@@ -16,24 +16,27 @@ export default function PremiumOffers() {
   const [Items, setItems] = useState("");
   const [slug, setSlug] = useState("");
   let params = useParams();
-  
-  const filterBook = Items !== '' ? Items.data.filter((books) => {
-    return (
-      books.name.toLocaleLowerCase().indexOf(searchItems.toLowerCase()) !== -1
-    );
-  }): '';
+
+  const filterBook =
+    Items !== ""
+      ? Items.data.filter((books) => {
+          return (
+            books.name
+              .toLocaleLowerCase()
+              .indexOf(searchItems.toLowerCase()) !== -1
+          );
+        })
+      : "";
 
   useEffect(() => {
     setSlug(params.offerid);
     OfferBuffets(params.offerid);
-  }, []);
-
-  
+  }, [params.offerid]);
 
   const OfferBuffets = async (id) => {
     let Result = await axios.get(
-      `https://dev.apps-foundry.com/scoopcor/api/v1/offers/items?offer_id=${id}&item_type=2`,
-      { headers: { Authorization: Cookies.get('token') } }
+      `${process.env.REACT_APP_BASE_URL}offers/items?offer_id=${id}&item_type=2`,
+      { headers: { Authorization: Cookies.get("token") } }
     );
 
     if (Result) {
@@ -60,26 +63,26 @@ export default function PremiumOffers() {
               />
             </form>
             <div className="flex grid lg:grid-cols-6 xl:grid-cols-5 lg:gap-0 xl:gap-6 md:grid-cols-3 md:gap-10 sm:grid-cols-3 grid-cols-1 gap-8 mx-14 sm:mx-auto">
-              {!isLoading
-                ? Items
-                  ? filterBook.map((books) => {
-                      return (
-                        <CardItem
-                          image={books.images}
-                          title={books.name}
-                          author={books.id}
-                          url={books.images}
-                          key={books.id}
-                          pageCount={books.page_count}
-                        />
-                      );
-                    })
-                  : ""
-                : 
-                    <div>
-                      Loading...
-                    </div>
-                }
+              {!isLoading ? (
+                Items ? (
+                  filterBook.map((books) => {
+                    return (
+                      <CardItem
+                        image={books.images}
+                        title={books.name}
+                        author={books.id}
+                        url={books.images}
+                        key={books.id}
+                        pageCount={books.page_count}
+                      />
+                    );
+                  })
+                ) : (
+                  ""
+                )
+              ) : (
+                <div>Loading...</div>
+              )}
             </div>
           </div>
         </div>
