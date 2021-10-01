cd859de55a9e5bbc337ff64fedf389e4a8a085a0
jymiko <jymiko92@gmail.com>
2021-10-01 15:18:54 +0700

    update sso my value

--- src/pages/Authorized.js
+++ src/pages/Authorized.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const Authorized = () => {
     const [accessToken, setAccessToken] = useState();
@@ -27,11 +28,63 @@ const Authorized = () => {
     }
 
     let getUserAPI = async(token) => {
-        let payloadData = token ? token : "";
-        axios.post('http://localhost/auth/valid', payloadData)
-        .then(response => console.log(response))
+        let payloadData = {
+            "access_token": token ? token : ""
+        }
+        axios.post('https://dev.apps-foundry.com/scoopcor/api/v1/auth/myvalue', payloadData)
+        .then(response => {
+            var res = response.data
+            console.log(res)
+            Cookies.set("token", res.realm.toUpperCase() + " " + res.access_token);
+            // Cookies.get("token")
+            Cookies.set("username", res.first_name);
+            Cookies.set("email", res.email)
+            if(response.status === 200){
+                ownedBuffet();
+            }else if(response.status === 201){
+                alert('user anda belum terdaftar');
+            }
+            
+            })
         .catch(err => console.log(err))
     }
+
+    const ownedBuffet = () => {
+        axios.get("https://dev.apps-foundry.com/scoopcor/api/v1/owned_buffets",{
+          headers: {Authorization:Cookies.get('token')}
+        })
+        .then((res)=>{
+          var countOnBuffets = res.data.metadata.resultset.count
+          var data =  res.data.owned_buffets;
+          var offerId;
+          var packageName;
+          var offer = [];
+          if(countOnBuffets > 0){
+            data.map((item, index)=>{
+              offerId = item.offerbuffet.offer.id
+              packageName = item.offerbuffet.offer.name
+              offer.push({
+                "offerId": offerId,
+                "name": packageName,
+              })
+              return offer
+            })
+            Cookies.set("offer", JSON.stringify(offer));
+            // var offerVar = setOffer(JSON.stringify(offer));
+            // dispatch(JSON.parse(offerVar));
+            // setLoading(false);
+            window.location.href = "/dashboard";
+          }else{
+            // setLoading(false);
+            window.location.href = "/dashboard";
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          alert(JSON.stringify(err));
+        //   setLoading(false);
+        });
+      }
 
     useEffect(() => {
         changeToken(params);
